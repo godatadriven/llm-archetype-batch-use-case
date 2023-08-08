b3e2519fe26891d2440144ee953017caaaf90aed
ykerus <yke.rusticus@gmail.com>
2023-08-08 10:11:50 +0200

    Add openai models

--- src/llm_batch/model.py
+++ src/llm_batch/model.py
@@ -1,32 +1,59 @@
 import logging
+import os
+
+import dotenv
+import openai
+from langchain.llms import AzureOpenAI
+
+dotenv.load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_base = os.getenv("OPENAI_API_BASE")
+openai.api_type = os.getenv("OPENAI_API_TYPE")
+openai.api_version = os.getenv("OPENAI_API_VERSION")
 
 logger = logging.getLogger(__name__)
 
 
 def get_llm(model_name: str):
-    logger.info(f"Loading LLM '{model_name}'")
-
-    if model_name == "gpt4":
-        llm = get_gpt4()
-
-    elif model_name == "gpt3":
-        llm = get_gpt3()
-
-    elif model_name == "palm2":
+    if model_name == "gpt-4":
+        llm = get_gpt_4()
+    elif model_name == "gpt-3":
+        llm = get_gpt_35_turbo()
+    elif model_name == "palm-2":
         llm = get_palm2()
-
     else:
         raise ValueError(f"Invalid model: {model_name}")
 
     return llm
 
 
-def get_gpt4():
-    return lambda x: x
+def get_gpt_4():
+    logger.info("Loading LLM 'gpt-4'")
+    return AzureOpenAI(
+        model_name="gpt-4",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1.0,
+        verbose=False,
+        engine="gpt-4-us",
+    )
 
 
-def get_gpt3():
-    ...
+def get_gpt_35_turbo():
+    # Different approach than gpt-4, as for gpt-3 we get an error:
+    # "The completion operation does not work with the specified model, gpt-35-turbo."
+    logger.info("Loading LLM 'gpt-35-turbo'")
+
+    return lambda prompt: openai.ChatCompletion.create(
+        model="gpt-35-turbo",
+        engine="gpt-35-turbo-us",
+        messages=[{"role": "user", "content": prompt}],
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+    )["choices"][0]["message"]["content"]
 
 
 def get_palm2():
