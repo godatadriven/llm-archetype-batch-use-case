5a09c1d58aeb60f72500520ae5c37cedfe392c23
ykerus <yke.rusticus@gmail.com>
2023-08-09 10:44:28 +0200

    Support yml schema instead of json

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 General solution to an archetype batch use case for LLMs.
 For a given set of input documents (pdf or txt), we apply an LLM to extract the relevant information and store it in a structured format (json).
 
-Update the `schema.json` file to your needs, upload your documents in `data/input_txt` and run `poetry run python src/llm_batch/main.py`.
+Configure your `.env`, update the `schema.yml` file to your needs, upload your documents in `data/input_txt` and run `poetry run python src/llm_batch/main.py`.
--- src/llm_batch/schema.py
+++ src/llm_batch/schema.py
@@ -1,33 +1,47 @@
-import json
+import logging
 from pathlib import Path
 
+import yaml  # type: ignore
 from langchain.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field, create_model
 
+logger = logging.getLogger(__name__)
+
+# Using type mapping to avoid using eval
 TYPE_MAPPING = {
     "str": str,
     "int": int,
+    "float": float,
+    "bool": bool,
     "list[str]": list[str],
     "list[int]": list[int],
+    "list[float]": list[float],
+    "list[bool]": list[bool],
 }
 
 
-def load_schema(schema_path: Path) -> BaseModel:
+def load_schema(schema_path: Path) -> type[BaseModel]:
+    logger.info(f"Loading schema from '{schema_path}'")
     with open(schema_path) as f:
-        schema = json.load(f)
+        schema = yaml.safe_load(f)
 
     fields = {}
     for field_name, field_info in schema.items():
-        field_type = field_info["type"]
+        _field_type = field_info["type"]
         field_description = field_info["description"]
-        field_type = TYPE_MAPPING.get(field_type)
+        field_type = TYPE_MAPPING.get(_field_type)
+
+        if field_type is None:
+            raise ValueError(
+                f"Unsupported type '{_field_type}' for field '{field_name}'"
+            )
 
         fields[field_name] = fields[field_name] = (
             field_type,
             Field(default=None, description=field_description),
         )
 
-    schema_model = create_model("SchemaModel", **fields)  # type: ignore
+    schema_model = create_model("SchemaModel", **fields)
     return schema_model
 
 
